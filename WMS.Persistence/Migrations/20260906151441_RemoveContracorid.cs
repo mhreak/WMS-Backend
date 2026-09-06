2c788b5cff8e9e8464f02f6ce87792c007c66275
alinati08 <ali.nati080@gmail.com>
2026-09-06 18:45:37 +0330

    Remove ContractorId from ExtraOrDeductionRule
    - Dropped the ContractorId column from the ExtraOrDeductionRule table.
    - Removed associated foreign key and index for ContractorId in the migration.
    - Updated the AppDbContextModelSnapshot to reflect the removal of ContractorId.

---
+++ WMS.Persistence/Migrations/20260906151441_RemoveContracorid.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveContracorid : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExtraOrDeductionRule_Contractor_ContractorId",
+                table: "ExtraOrDeductionRule");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ExtraOrDeductionRule_ContractorId",
+                table: "ExtraOrDeductionRule");
+
+            migrationBuilder.DropColumn(
+                name: "ContractorId",
+                table: "ExtraOrDeductionRule");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ContractorId",
+                table: "ExtraOrDeductionRule",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ExtraOrDeductionRule_ContractorId",
+                table: "ExtraOrDeductionRule",
+                column: "ContractorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExtraOrDeductionRule_Contractor_ContractorId",
+                table: "ExtraOrDeductionRule",
+                column: "ContractorId",
+                principalTable: "Contractor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
