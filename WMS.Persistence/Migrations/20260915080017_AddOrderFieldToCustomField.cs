be219093389951776a124f4e89d20254ac3521fd
alinati08 <ali.nati080@gmail.com>
2026-09-15 11:38:10 +0330

    Add order fields and visibility flag to EntityCustomField
    - Introduced new columns: Filter_ShowOrder, Form_ShowOrder, Grid_ShowOrder (type: smallint) to the EntityCustomField table.
    - Added ShowInGrid column (type: bit) to indicate visibility in grid.
    - Updated AppDbContextModelSnapshot to reflect these changes.

---
+++ WMS.Persistence/Migrations/20260915080017_AddOrderFieldToCustomField.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WMS.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderFieldToCustomField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<short>(
+                name: "Filter_ShowOrder",
+                table: "EntityCustomField",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Form_ShowOrder",
+                table: "EntityCustomField",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Grid_ShowOrder",
+                table: "EntityCustomField",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ShowInGrid",
+                table: "EntityCustomField",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Filter_ShowOrder",
+                table: "EntityCustomField");
+
+            migrationBuilder.DropColumn(
+                name: "Form_ShowOrder",
+                table: "EntityCustomField");
+
+            migrationBuilder.DropColumn(
+                name: "Grid_ShowOrder",
+                table: "EntityCustomField");
+
+            migrationBuilder.DropColumn(
+                name: "ShowInGrid",
+                table: "EntityCustomField");
+        }
+    }
+}
